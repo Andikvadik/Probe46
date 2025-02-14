3a709ffa064bb4c6967e7ab5faca4e96b8b98c25
Andikvadik <andriankaxuliganka@gmail.com>
2025-02-14 03:39:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Точно?",
+    "Ти впевнений??",
+    "Добре подумай?",
+    "А подумай ще...",
+    "Будь-ласка!!!",
+    "Якщо ти відмовиш, мені буде дуже сумно...",
+    "Буде дууже сумно...",
+    "Я буду плакати...",
+    "Ну, добре давай тисни...",
+    "Клац! ❤️"
 ];
 
 let messageIndex = 0;
